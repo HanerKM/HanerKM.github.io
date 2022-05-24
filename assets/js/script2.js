2cd524ee71fb521deb3161af853da501a6838236
HanerKM <99375111+HanerKM@users.noreply.github.com>
2022-05-24 01:48:11 -0400

    Initial build

---
+++ assets/js/script2.js
@@ -0,0 +1,8 @@
+// const card = document.getElementById('flex-inner')
+
+// card.addEventListener('click', flipCard)
+
+// function flipCard() {
+//   card.classList.toggle('flipacard')
+// }
+//No longer needed, solved with pure CSS
